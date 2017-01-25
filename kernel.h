dfb86378c24b52d8d1f4615bc8aed915d711915e
Keshav <keshavr@uw.edu>
2017-01-25 14:32:27 -0800

    aster

---
+++ kernel.h
@@ -0,0 +1,11 @@
+#ifndef KERNEL_H_INCLUDED
+#define KERNEL_H_INCLUDED
+
+void scale(float *inp, int len, float *out, float scale);
+
+void vecAdd(float *inp1, int len1, float *inp2, int len2,float *out);
+
+void compProd(float *inp1, int len1, float *inp2, int len2, float *out);
+
+
+#endif
